0579852ee4ac222673bbdf5e64a788728bb88e1b
shahriar201 <shahriarbrine201@gmail.com>
2021-08-24 09:10:50 +0600

    Connection with backend

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Check: {{ apiValue }}</h1>
+    <button @click.prevent="testBackendRequest">Request API</button>
+    <Tutorial/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data(){
+    return {
+      apiValue: ''
+    }
+  },
+
+  mounted(){
+    // this.testBackendRequest();
+  },
+
+  methods:{
+    testBackendRequest() {
+      this.$axios.get('/test').then(res => {
+        // console.log(res.data);
+        this.apiValue = res.data;
+      });
+    }
+  },
+  
+
+});
+</script>
