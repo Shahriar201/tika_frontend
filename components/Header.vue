cec6db66eefee40cba1ad03b7cc22bbdb5841e18
shahriar201 <shahriarbrine201@gmail.com>
2021-09-29 20:11:02 +0600

    Heading and banner setup

--- components/Header.vue
+++ components/Header.vue
@@ -1,58 +1,87 @@
 <template>
-    <div>
-        <div class="primary-bg">
-            <div class="container mx-auto py-20">
-                <div class="bg-white mb-20">
-                    <div class="flex items-center">
-                        <div class="min-w-max">
-                            <h2 class="text-2xl font-bold text-black uppercase px-6">TIKA</h2>
-                        </div>
-
-                        <div class="w-full">
-                            <ul class="flex justify-center">
-                                <li><luxt-link class="py-10 px-5 inline-block" to="">Home</luxt-link></li>
-                                <li><luxt-link class="py-10 px-5 inline-block" to="">Registration</luxt-link></li>
-                                <li><luxt-link class="py-10 px-5 inline-block" to="">Verity</luxt-link></li>
-                                <li><luxt-link class="py-10 px-5 inline-block" to="">Tika Card</luxt-link></li>
-                                <li><luxt-link class="py-10 px-5 inline-block" to="">Help</luxt-link></li>
-                                <li><luxt-link class="py-10 px-5 inline-block" to="">Faqs</luxt-link></li>
-                            </ul>
-                        </div>
-
-                        <div class="min-w-max">
-                            <div class="px-6">
-                                <nuxt-link class="primary-bg text-2xl text-white px-6 py-2 rounded-2xl" to="/">Register Now</nuxt-link>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="flex items-center">
-                    <div class="w-full">
-                        <h1 class="hero-heading font-bold text-white uppercase leading-none mb-6">Vaccine</h1>
-                        <h2 class="text-7xl font-medium text-white mb-10">Registration</h2>
-                        <!-- <p class="text-white text-2xl">Complete the registration by verifying your national identity card and mobile number</p> -->
-                    </div>
-
-                    <div class="min-w-max">
-                      <img src="/static/img/bro.png" alt="" class="">
-                    </div>
-                </div>
-
+  <div>
+    <div class="primary-bg">
+      <div class="container mx-auto py-20">
+        <div class="bg-white mb-20">
+          <div class="flex items-center">
+            <div class="min-w-max">
+              <h2 class="text-2xl font-bold text-black uppercase px-6">TIKA</h2>
             </div>
+
+            <div class="w-full">
+              <ul class="flex justify-center">
+                <li>
+                  <luxt-link class="py-10 px-5 inline-block" to=""
+                    >Home</luxt-link
+                  >
+                </li>
+                <li>
+                  <luxt-link class="py-10 px-5 inline-block" to=""
+                    >Registration</luxt-link
+                  >
+                </li>
+                <li>
+                  <luxt-link class="py-10 px-5 inline-block" to=""
+                    >Verity</luxt-link
+                  >
+                </li>
+                <li>
+                  <luxt-link class="py-10 px-5 inline-block" to=""
+                    >Tika Card</luxt-link
+                  >
+                </li>
+                <li>
+                  <luxt-link class="py-10 px-5 inline-block" to=""
+                    >Help</luxt-link
+                  >
+                </li>
+                <li>
+                  <luxt-link class="py-10 px-5 inline-block" to=""
+                    >Faqs</luxt-link
+                  >
+                </li>
+              </ul>
+            </div>
+
+            <div class="min-w-max">
+              <div class="px-6">
+                <nuxt-link
+                  class="primary-bg text-2xl text-white px-6 py-2 rounded-2xl"
+                  to="/"
+                  >Register Now</nuxt-link
+                >
+              </div>
+            </div>
+          </div>
         </div>
+
+        <div class="flex items-center">
+          <div class="w-full">
+            <h1
+              class="hero-heading font-bold text-white uppercase leading-none mb-6"
+            >
+              Vaccine
+            </h1>
+            <h2 class="text-7xl font-medium text-white mb-10">Registration</h2>
+            <p class="text-white text-2xl">
+              Complete the registration by verifying your national identity card
+              and mobile number
+            </p>
+          </div>
+
+          <div class="min-w-max">
+            <img src="img/bro.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    setup() {
-
-    },
-}
+  setup() {}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
